f9c542461afceff42d52f2207f6d0de4df738254
Ayeni Clement <ayeniayokunle@gmail.com>
2022-05-30 12:47:12 +0100

    i just  added the profivder.tf file  for the build docker container project

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  required_providers {
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+
+    }
+  }
+}
+provider "docker" {
+
+  host    = "npipe:////.//pipe//docker_engine"
+}
+#provider "docker" {
+  #host = "unix:///var/run/docker.sock"
+
+#}
